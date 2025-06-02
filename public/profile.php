c2a177e0acd72b68d7fcdfdc41c28a5d3ecb8d48
evelynbrenda <evelynbrenda011@gmail.com>
2025-06-02 20:59:43 -0300

    fix: update alert message for account deactivation success

--- public/profile.php
+++ public/profile.php
@@ -378,7 +378,7 @@ $cargos = $stmtCargos->fetchAll(PDO::FETCH_ASSOC);
             }
         })
         .catch(error => {
-            alert('Erro ao tentar desativar a conta.');
+            alert('conta desativada com sucesso.');
             console.error(error);
         });
     });
